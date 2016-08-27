be0eebb03e5f4d54d5d192151f2155bf440e196d
shubham93 <shubham.uniyal@gmail.com>
2016-08-27 16:46:00 +0530

    databse connected

--- esender/models.py
+++ esender/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class email_data(models.Model):
+    subject=models.TextField()
+    message=models.TextField()
+    email=models.TextField()
--- esender/views.py
+++ esender/views.py
@@ -4,23 +4,31 @@ from django.core.mail import send_mail
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.template.loader import get_template
-
+from esender.models import email_data
 def email(request):
     errors = []
+    email_list=[]
     from_email=settings.EMAIL_HOST_USER
     if request.method == 'POST':
-        if not request.POST.get('subject', ''):
+        if request.POST['subject'] =='':
             errors.append('Enter a subject.')
-        if not request.POST.get('message', ''):
+        if request.POST['message'] =='':
             errors.append('Enter a message.')
-        if request.POST.get('email') and '@' not in request.POST['email']:
+        if request.POST['email'] and '@' not in request.POST['email']:
             errors.append('Enter a valid e-mail address.')
+        email_list=request.POST['email'].split(",")
+        # save info in db
+        p=email_data()
+        p.subject=request.POST['subject']
+        p.email=request.POST['email']
+        p.message=request.POST['message']
+        p.save()
+        # #######################
+        #print email_list
         if not errors:
           try:
-              
-            send_mail(request.POST['subject'],request.POST['message'],from_email,request.POST('email'),fail_silently=False)
+            send_mail(request.POST['subject'],request.POST['message'],from_email, email_list,fail_silently=False)
             return HttpResponse('Thank you, form has been submitted successfully')
-          except Exception as err: 
+          except Exception as err:
             return HttpResponse(str(err))
-    return render(request, 'contact.html',
-        {'errors': errors})
+    return render(request, 'contact.html',{'errors': errors})
